84bcb91a05b7ed98557a26584bb70e8ad0cb817c
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2024-08-17 12:13:05 -0400

    Installed shadcn button component

--- booking/app/page.tsx
+++ booking/app/page.tsx
@@ -1,8 +1,14 @@
+import { Button } from "@/components/ui/button";
+
 
 
 const HomePage = () => {
+
   return (
-    <h1 className="text-3xl">Home Page</h1>
+    <div className="">
+      <h1 className="text-3xl">Home Page</h1>
+      <Button variant='destructive' size={"icon"} className=" capitalize m-8">Book</Button>
+    </div>
   )
 }
 export default HomePage;
