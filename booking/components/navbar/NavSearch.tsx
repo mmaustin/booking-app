dcef0a2bc2f861d7462589dd5d63f6f7677f427a
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2024-08-17 17:23:06 -0400

    Changed shadcn theme values

--- booking/components/navbar/NavSearch.tsx
+++ booking/components/navbar/NavSearch.tsx
@@ -1,9 +1,8 @@
-
-
+import { Input } from "../ui/input";
 
 const NavSearch = () => {
   return (
-    <div>NavSearch</div>
+    <Input className="max-w-xs dark:bg-muted" type='text' placeholder="find a property . . . " />
   )
 }
 export default NavSearch;
