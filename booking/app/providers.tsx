9756e0b46d25ce51d708bc50b01fbc83c7efdc03
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2024-08-18 11:09:32 -0400

    Imported theme provided code from shadcn/next theme documentation and wrapped provider children

--- booking/app/providers.tsx
+++ booking/app/providers.tsx
@@ -1,9 +1,18 @@
 'use client';
 
+import { ThemeProvider } from "./theme-provider";
+
 
 function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <>{children}</>
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
+      <>{children}</>
+    </ThemeProvider>
   )
-}
+};
 export default Providers;
