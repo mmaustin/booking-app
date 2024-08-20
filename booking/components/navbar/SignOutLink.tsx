3df919f9dd6caf6ebaa24f5d2ff0b9c85192499a
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2024-08-20 15:23:29 -0400

    Built out the signoutlink component with toast and clerk signoutbutton

--- booking/components/navbar/SignOutLink.tsx
+++ booking/components/navbar/SignOutLink.tsx
@@ -1,9 +1,21 @@
+'use client'
 
-
+import { SignOutButton } from "@clerk/nextjs";
+import { useToast } from "../ui/use-toast";
 
 const SignOutLink = () => {
+
+  const {toast} = useToast();
+  const handleLogout = () => {
+    toast({description: "You have logged out."});
+  }
+
   return (
-    <div>SignOutLink</div>
+    <SignOutButton redirectUrl="/">
+      <button className="w-full text-left" onClick={handleLogout}>
+Logout
+      </button>
+    </SignOutButton>
   )
 }
 export default SignOutLink;
