6a80be4e24556ed731c6484a3a280106bc1c323c
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2024-08-20 15:49:33 -0400

    Added register, signin, and signout functions to linksdropdown component

--- booking/components/navbar/LinksDropdown.tsx
+++ booking/components/navbar/LinksDropdown.tsx
@@ -11,7 +11,8 @@ import { Button } from '../ui/button';
 import UserIcon from './UserIcon';
 import { links } from '@/utils/links';
 import SignOutLink from './SignOutLink';
-
+import { currentUser } from '@clerk/nextjs/server';
+import { SignedIn, SignedOut, SignUpButton, SignInButton } from '@clerk/nextjs';
 
 const LinksDropdown = () => {
   return (
@@ -23,13 +24,33 @@ const LinksDropdown = () => {
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent className='w-52' align='start' sideOffset={10}>
-        {links.map((link) => {
+        <SignedOut>
+          <DropdownMenuItem>
+            <SignInButton mode='modal' >
+    <button className='w-full text-left'>Login</button>
+            </SignInButton>
+          </DropdownMenuItem>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>
+            <SignUpButton mode='modal' >
+    <button className='w-full text-left'>Register</button>
+            </SignUpButton>
+          </DropdownMenuItem>
+        </SignedOut>
+        <SignedIn>
+          {links.map((link) => {
           return (
             <DropdownMenuItem key={link.href}>
               <Link className='capitalize w-full' href={link.href} >{link.label}</Link>
             </DropdownMenuItem>
           )
         })}
+        <DropdownMenuSeparator/>
+        <DropdownMenuItem>
+          <SignOutLink/>
+        </DropdownMenuItem>
+        </SignedIn>
+        
       </DropdownMenuContent>
     </DropdownMenu>
   )
