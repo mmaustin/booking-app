fd513b080540e5cdbbfc0e28360a5a9674c76b5c
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2024-08-24 16:25:29 -0400

    Refactored create profile page to incorporate form components

--- booking/app/profile/create/page.tsx
+++ booking/app/profile/create/page.tsx
@@ -1,11 +1,12 @@
+import { SubmitButton } from "@/components/form/Buttons";
+import FormContainer from "@/components/form/FormContainer";
 import FormInput from "@/components/form/FormInput";
-import { Button } from "@/components/ui/button"
 
-const createProfileAction = async (formData: FormData) => {
+
+const createProfileAction = async (prevState: any, formData: FormData) => {
   'use server'
   const firstName = formData.get('firstName') as string;
-  console.log(firstName);
-
+  return {message: 'Profile Created'}
 }
 
 const CreateProfilePage = async () => {
@@ -13,9 +14,15 @@ const CreateProfilePage = async () => {
   return (
     <section>
       <h1 className="text-2xl font-semibold mb-8 capitalize">new user</h1>
-      <div className="border p-8 rounded-md max-w-lg">
-        <form action={createProfileAction}>
-        </form>
+      <div className="border p-8 rounded-md">
+        <FormContainer action={createProfileAction}>
+          <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4 mt-4">
+          <FormInput type="text" name="firstName" label="First Name"/>
+          <FormInput type="text" name="lastName" label="Last Name"/>
+          <FormInput type="text" name="username" label="Username"/>
+          </div>
+          <SubmitButton text="Create Profile" className="mt-8" />
+        </FormContainer>
       </div>
     </section>
   )
