263e6aaa4b1e68cf9ae823cea63bd5d9b15664bf
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2024-08-24 11:55:25 -0400

    Created profile/create page for redirected signed in user

---
+++ booking/app/profile/create/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+
+const CreateProfilePage = async() => {
+
+  
+
+  return (
+    <div>CreateProfilePage</div>
+  )
+}
+export default CreateProfilePage
