254d9a2f5244af0ad5b1a26af8603e6c68e4b23b
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2024-08-24 12:26:18 -0400

    Tested profile page formdata functionality

--- booking/app/profile/create/page.tsx
+++ booking/app/profile/create/page.tsx
@@ -2,12 +2,28 @@ import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 
-const CreateProfilePage = async() => {
+const createProfileAction = async (formData: FormData) => {
+  'use server'
+  const firstName = formData.get('firstName') as string;
+  console.log(firstName);
 
-  
+}
+
+const CreateProfilePage = async () => {
 
   return (
-    <div>CreateProfilePage</div>
+    <section>
+      <h1 className="text-2xl font-semibold mb-8 capitalize">new user</h1>
+      <div className="border p-8 rounded-md max-w-lg">
+        <form action={createProfileAction}>
+          <div className="mb-2">
+            <Label htmlFor="firstName">First Name</Label>
+            <Input id="firstName" name="firstName" type="text"></Input>
+          </div>
+          <Button type="submit" size="lg">Create Profile</Button>
+        </form>
+      </div>
+    </section>
   )
 }
 export default CreateProfilePage
