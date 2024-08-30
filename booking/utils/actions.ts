47cf4ed09af8971a9011d984fc1d08c185b2029a
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2024-08-30 15:47:35 -0400

    Provided values for the prisma profile.create method

--- booking/utils/actions.ts
+++ booking/utils/actions.ts
@@ -1,15 +1,31 @@
 'use server';
 
 import { profileSchema } from "./schemas";
+import db from './db';
+import { auth, clerkClient, currentUser } from '@clerk/nextjs/server';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
 
 export const createProfileAction = async (prevState: any, formData: FormData) => {
   try {
+    const user = await currentUser();
+    if (!user) throw new Error('No user exists.')
+
     const rawData = Object.fromEntries(formData);
     const validatedFields = profileSchema.parse(rawData);
-    console.log(validatedFields);
-    return {message: 'profile created'}
+
+    await db.profile.create({
+      data: {
+        clerkId: user.id,
+        email: user.emailAddresses[0].emailAddress,
+        profileImage: user.imageUrl ?? '',
+        ...validatedFields
+      }
+    })
+
+    return { message: 'profile created' }
   } catch (error) {
     console.log(error);
-    return {message: 'there was an error'};
+    return { message: 'there was an error' };
   };
 };
