3a27815e9424a084ae75f9456fa4770da2ee5736
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2024-09-03 07:57:59 -0400

    Used a helper function to grab current user in fetchprofile action

--- booking/utils/actions.ts
+++ booking/utils/actions.ts
@@ -6,6 +6,16 @@ import { auth, clerkClient, currentUser } from '@clerk/nextjs/server';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 
+const getAuthUser = async () => {
+  const user =  await currentUser();
+  if(!user) {
+    throw new Error('You must login to access this route.');
+  };
+
+  if(!user.privateMetadata.hasProfile) redirect('/profile/create');
+  return user;
+};
+
 export const createProfileAction = async (prevState: any, formData: FormData) => {
   try {
     const user = await currentUser();
@@ -48,4 +58,16 @@ export const fetchProfileImage = async () => {
   });
 
   return profile?.profileImage;
+};
+
+export const fetchProfile = async () => {
+  const user = await getAuthUser();
+
+  const profile = await db.profile.findUnique({
+    where: {
+      clerkId: user.id,
+    },
+  });
+  if (!profile) return redirect('/profile/create');
+  return profile;
 };
