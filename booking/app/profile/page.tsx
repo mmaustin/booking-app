46b12bb1460cfdf48d939d37967504fb00f081f1
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2024-09-05 07:21:57 -0400

    Added initial tsx in profile page

--- booking/app/profile/page.tsx
+++ booking/app/profile/page.tsx
@@ -4,8 +4,22 @@ import FormInput from '@/components/form/FormInput';
 import { SubmitButton } from '@/components/form/Buttons';
 
 
-const ProfilePage = () => {
+const ProfilePage = async () => {
+  const user = await fetchProfile();
+
   return (
-    <div>Profile Page</div>
+    <section>
+      <h1 className="text-2xl font-semibold mb-8 capitalize">user profile</h1>
+      <div className="border p-8 rounded-md">
+        <FormContainer action={updateProfileAction}>
+          <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4 mt-4">
+            <FormInput type="text" name="firstName" label="First Name" defaultValue={user.firstName} />
+            <FormInput type="text" name="lastName" label="Last Name" defaultValue={user.lastName} />
+            <FormInput type="text" name="username" label="Username" defaultValue={user.username} />
+          </div>
+          <SubmitButton text="Create Profile" className="mt-8" />
+        </FormContainer>
+      </div>
+    </section>
   )
 };
